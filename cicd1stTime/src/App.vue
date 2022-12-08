f4d0604eb3040ba20307b10ff4d449f091b60811
avlachos <adam.vlachos78@gmail.com>
2022-12-08 16:29:24 +0200

    1st attempt 2 commit

---
+++ cicd1stTime/src/App.vue
@@ -0,0 +1,14 @@
+<script lang="ts">
+import NewComponent4CICD from "@/components/NewComponent4CICD/NewComponent4CICD.vue"; //What does this do?
+
+export default {
+  components: {NewComponent4CICD}
+}//What does this do?
+</script>
+
+<template>
+  <NewComponent4CICD></NewComponent4CICD>
+</template>
+
+<style scoped>
+</style>
---
+++ cicd1stTime/src/components/NewComponent4CICD/NewComponent4CICD.vue
@@ -0,0 +1,24 @@
+<script>
+import {defineComponent} from "vue"; //What does this do?
+
+export default defineComponent({
+  name: "NewComponent4CICD",
+  StudyBuddyGroupNames: ["Ioanna", "David", "Adam"]
+})//What does this do?
+
+</script>
+
+<template>
+  <h1>JUST A TITLE!</h1>
+<!--  <ol>-->
+<!--    <li>-->
+<!--          -->
+<!--    </li>-->
+<!--  </ol>-->
+<!--  for ( ++-->
+</template>
+
+
+<style scoped>
+
+</style>
